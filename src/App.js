db6fb98ba8f307dc66f9a22edf04e5020299dae3
Saba <sabarht@gmail.com>
2023-06-24 13:07:54 +0200

    installing uid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 import Form from "./components/form";
+import React, { useState } from "react";
+import { uid } from "uid";
+
 function App() {
+  const [activities, setActivities] = useState();
+  function handleAddActivity(newActivity) {
+    setActivities([...activities, { ...newActivity, id: uid() }]);
+  }
   return (
     <div>
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
+      <p>{activities}</p>
     </div>
   );
 }

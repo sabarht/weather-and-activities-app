cef1a22be43267d2acf212ffd8a3268c2ea59990
Saba <sabarht@gmail.com>
2023-06-26 10:36:05 +0200

    adding local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Form from "./components/form";
-import React, { useState } from "react";
+import React from "react";
 import { uid } from "uid";
 import List from "./components/list";
+import useLocalStorageState from "use-local-storage-state";
 
+// const isGoodWeather = true;
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
+
   function handleSubmit(newActivity) {
     setActivities([...activities, { ...newActivity, id: uid() }]);
   }
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,9 +1,8 @@
 export default function List({ activities }) {
-  console.log({ activities });
   return (
     <ul>
-      {activities.map((activity) => (
-        <li>{activity.name}</li>
+      {activities?.map((activity) => (
+        <li key={activity.id}>{activity.name}</li>
       ))}
     </ul>
   );
